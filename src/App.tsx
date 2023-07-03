bc015b326f65ce96ba604ee9c06201951d376fd6
Maxim Dmitriev <Renissance@tut.by>
2023-07-03 11:00:42 +0300

    feat: add button css

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ export function App() {
     <>
       <div style={{ margin: 20 }}>
         <Button>Button primary</Button>
+        <Button variant={'secondary'}>Button secondary</Button>
+        <Button variant={'tertiary'}>Tertiary</Button>
+        <Button as="a" variant={'link'} href={'/'}>
+          Link-button
+        </Button>
         <button>aaaa</button>
       </div>
     </>
