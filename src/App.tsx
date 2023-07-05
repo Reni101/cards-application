5bd12221dff84893b09d56228ddf83b8b2050795
Kirill <apatski94@gmail.com>
2023-07-05 12:27:50 +0300

    feat: add local state (test) and TextField

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+
 import { LogOut } from './assets/icons/logOut'
 import { Button } from './components/ui/button'
+import { TextField } from './components/ui/text-field'
 
 export function App() {
+  const [value, setValue] = useState('')
+
   return (
     <>
       <div style={{ margin: 20 }}>
         <Button variant={'secondary'}>
           <LogOut /> Primary
         </Button>
+        <TextField value={value} onSetValue={setValue} type="password" label="Input" />
       </div>
     </>
   )
