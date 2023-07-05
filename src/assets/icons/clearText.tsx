d19197e00f7f10eb9b0529a6f4423c2a7e1447c9
Kirill <apatski94@gmail.com>
2023-07-05 16:30:22 +0300

    fix: fix style in fill

--- src/assets/icons/clearText.tsx
+++ src/assets/icons/clearText.tsx
@@ -3,13 +3,13 @@ const ClearText = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" ref={ref} {...props}>
     <g clipPath="url(#a)">
       <path
-        fill="#fff"
+        fill="currentColor"
         d="m8.94 8 2.87-2.86a.67.67 0 0 0-.95-.95L8 7.06 5.14 4.19a.67.67 0 0 0-.95.95L7.06 8l-2.87 2.86A.67.67 0 0 0 4.67 12a.67.67 0 0 0 .47-.2L8 8.95l2.86 2.87a.67.67 0 1 0 .95-.95L8.94 8Z"
       />
     </g>
     <defs>
       <clipPath id="a">
-        <path fill="#fff" d="M0 0h16v16H0z" />
+        <path fill="currentColor" d="M0 0h16v16H0z" />
       </clipPath>
     </defs>
   </svg>
