66fcc14b9c35653e162522e1d8ad42bb76f14dbf
Maxim Dmitriev <Renissance@tut.by>
2023-07-06 09:23:29 +0300

    feat: add dropdown-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import s from './App.module.scss'
+import { DropdownMenu } from './components/ui/dropdownMenu'
 import { Header } from './components/ui/header'
 
 export function App() {
   return (
     <div className={s.container}>
       <Header />
+      <DropdownMenu options={['aaaaa', 'bbbbaaaaaaaaa']}>
+        <div style={{ color: 'red' }}>Trigger</div>
+      </DropdownMenu>
     </div>
   )
 }
