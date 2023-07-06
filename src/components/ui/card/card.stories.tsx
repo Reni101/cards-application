a7ca404275ab5d98a7ea529ba90a57ab8d82f66f
Maxim Dmitriev <Renissance@tut.by>
2023-07-06 15:56:49 +0300

    fix: fix card stories

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -16,6 +16,6 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    children: <div style={{ padding: '50px', fontSize: '40px' }}>Test</div>,
+    children: <div style={{ width: 200, height: 100 }}> Card wrapper</div>,
   },
 }
