aa8b420b40d6597d2b0ae41966e07c9cc13cd70b
Maxim Dmitriev <Renissance@tut.by>
2023-07-10 10:18:52 +0300

    fix:fix some naming

--- src/components/auth/login-form/login-form.stories.ts
+++ src/components/auth/login-form/login-form.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { LoginForm } from './'
 
 const meta = {
-  title: 'Auth/Login_form',
+  title: 'Auth/LoginForm',
   component: LoginForm,
   tags: ['autodocs'],
   argTypes: {
