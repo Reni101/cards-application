a7c6b1fa321cdbad6977d09fbe2ec183cc503bd1
Kirill <apatski94@gmail.com>
2023-07-11 17:16:31 +0300

    fix: put the code into separate components

--- src/components/auth/profile/profile.tsx
+++ src/components/auth/profile/profile.tsx
@@ -1,35 +1,34 @@
-import Edit from '../../../assets/icons/edit.tsx'
-import { LogOut } from '../../../assets/icons/logOut.tsx'
-import { Button } from '../../ui/button'
+import { useState } from 'react'
+
 import { Card } from '../../ui/card'
 import { Typography } from '../../ui/typography'
 
+import { EditProfileInfo } from './edit-profile-info/edit-profile-info.tsx'
+import { ProfileInfo } from './profile-info/profile-info.tsx'
 import s from './profile.module.scss'
+
 export const Profile = () => {
+  const [edit, setEdit] = useState(false)
+
   return (
-    <Card className={s.container}>
+    <Card className={`${s.container} ${edit && s.changeInfo}`}>
       <Typography variant="large" className={s.title}>
         Personal Information
       </Typography>
-      <div className={s.avatar}>
-        <Button className={s.editWrapper} variant="secondary">
-          <Edit />
-        </Button>
-      </div>
-      <div className={s.userInfo}>
-        <div className={s.userName}>
-          <Typography variant="h1" as="h1" className={s.name}>
-            Kirill
-          </Typography>
-          <Edit className={s.edit} />
-        </div>
-        <Typography variant="body2" className={s.email}>
-          test@gmail.com
-        </Typography>
-        <Button variant="secondary">
-          <LogOut /> Logout
-        </Button>
-      </div>
+      {edit ? (
+        <EditProfileInfo
+          avatar="https://zamanilka.ru/wp-content/uploads/2023/05/ava-baby-290523-2-1024x1024.jpg"
+          onCloseEdit={setEdit}
+          name="Kirill"
+        />
+      ) : (
+        <ProfileInfo
+          onEdit={setEdit}
+          avatar="https://zamanilka.ru/wp-content/uploads/2023/05/ava-baby-290523-2-1024x1024.jpg"
+          email="test@gmail.com"
+          name="Kirill"
+        />
+      )}
     </Card>
   )
 }
