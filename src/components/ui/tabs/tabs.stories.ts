3d2a8adde62527dfa31aab9fceb06b134b55030c
Kirill <apatski94@gmail.com>
2023-07-20 14:24:55 +0300

    feat: add tabs stories

---
+++ src/components/ui/tabs/tabs.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Tabs } from './'
+
+const meta = {
+  title: 'Components/Tabs',
+  component: Tabs,
+  tags: ['autodocs'],
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000',
+        },
+      ],
+    },
+  },
+  argTypes: {
+    tabs: [
+      { value: '123', tabName: 'switcher' },
+      { value: '245', tabName: 'switcher 2' },
+    ],
+  },
+} satisfies Meta<typeof Tabs>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    tabs: [
+      { value: '123', tabName: 'switcher' },
+      { value: '245', tabName: 'switcher 2' },
+    ],
+  },
+}
+
+export const Desabled: Story = {
+  args: {
+    tabs: [
+      { value: '123', tabName: 'switcher' },
+      { value: '245', tabName: 'switcher 2', disabled: true },
+    ],
+  },
+}
