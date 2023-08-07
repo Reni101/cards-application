0c66d5643a347f58f4330d45aa1458a4183c4010
Maxim Dmitriev <Renissance@tut.by>
2023-08-07 12:05:58 +0300

    feat: add private routs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import s from './App.module.scss'
-import { LoginForm } from './components/auth/login-form'
-import { Header } from './components/ui/header'
 import { Router } from './router'
 
 export function App() {
-  return (
-    <>
-      <Header isLoggedIn={false} />
-      <div className={s.container}>
-        <LoginForm />
-      </div>
-
-      <Router />
-    </>
-  )
+  return <Router />
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,38 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom'
 
-const router = createBrowserRouter([
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
   {
     path: '/',
     element: <div>hello</div>,
   },
+]
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+  ...publicRoutes,
 ])
+
+function PrivateRoutes() {
+  const isAuthenticated = true
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
 
 export const Router = () => {
   return <RouterProvider router={router} />
