da6dd28c96a49aba012e50578d6026cc0953f9cc
Maxim Dmitriev <Renissance@tut.by>
2023-08-07 08:50:03 +0300

    feat: add table

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -6,39 +6,6 @@ import { Column, Sort, Thead } from './thead/thead'
 export const Table = () => {
   const [sort, setSort] = useState<Sort>(null)
 
-  const data = [
-    {
-      title: 'Pack name A',
-      cardsCount: 10,
-      updated: '2023-07-07',
-      createdBy: 'John Doe',
-    },
-    {
-      title: 'Pack name B',
-      cardsCount: 5,
-      updated: '2023-07-06',
-      createdBy: 'Jane Smith',
-    },
-    {
-      title: 'Pack name C',
-      cardsCount: 8,
-      updated: '2023-07-05',
-      createdBy: 'Alice Johnson',
-    },
-    {
-      title: 'Pack name D',
-      cardsCount: 3,
-      updated: '2023-07-07',
-      createdBy: 'Bob Anderson',
-    },
-    {
-      title: 'Pack name E',
-      cardsCount: 12,
-      updated: '2023-07-04',
-      createdBy: 'Emma Davis',
-    },
-  ]
-
   return (
     <table className={s.container}>
       <Thead
@@ -51,7 +18,7 @@ export const Table = () => {
       <tbody>
         {data.map(el => (
           <tr key={el.title}>
-            <td>{el.title}</td>
+            <td>{el.title + '1111'}</td>
             <td>{el.cardsCount}</td>
             <td>{el.updated}</td>
             <td>{el.createdBy}</td>
@@ -62,6 +29,39 @@ export const Table = () => {
     </table>
   )
 }
+const data = [
+  {
+    title: 'Pack name A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+  },
+  {
+    title: 'Pack name B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+  },
+  {
+    title: 'Pack name C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+  },
+  {
+    title: 'Pack name D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+  },
+  {
+    title: 'Pack name E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+  },
+]
+
 const dataHead: Column[] = [
   {
     key: 'name',
